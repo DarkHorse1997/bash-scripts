a8d0b01250af483ed862477fb86d96628ce760cb
Tanmoy Das <tanmoydas1997@gmail.com>
2019-04-05 03:10:15 +0530

    Added installer.sh

---
+++ installer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Getting Microsoft Repository key"
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+echo "Installing Visual Studio Code"
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+
+echo "Installing Calibre"
+
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+echo "Getting Miniconda"
+wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+bash Miniconda3-latest-Linux-x86_64.sh
+
